69c981fe109e7b0c278adb5819efa2c394ead9c7
richardmguckert <78433428+richardmguckert@users.noreply.github.com>
2022-06-19 07:51:02 -0300

    Add files via upload

---
+++ precache-manifest.de71189f2f09156b3b3d9c80a666d49e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c633db0a2a2c7f1661a",
+    "url": "/css/app.35be9204.css"
+  },
+  {
+    "revision": "c172dc5995d170d5607b",
+    "url": "/css/chunk-vendors.0e8d2a0e.css"
+  },
+  {
+    "revision": "279cfdde60624f67dc8454bde25a9e50",
+    "url": "/img/logo.279cfdde.svg"
+  },
+  {
+    "revision": "b29c75a1cfe073a744c45b4f60843d9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c633db0a2a2c7f1661a",
+    "url": "/js/app.55145268.js"
+  },
+  {
+    "revision": "c172dc5995d170d5607b",
+    "url": "/js/chunk-vendors.1b9d908e.js"
+  },
+  {
+    "revision": "6b6994b62c00b79e662cd1d6fe167c31",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
